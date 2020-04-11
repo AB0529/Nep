9dee8becf7f51dc36839b269e5d77a899e6251db
MoistSenpai <supercrab115@gmail.com>
2020-04-11 02:25:33 -0400

    Added empty play command and installed mongoose

---
+++ Commands/Music/play.ts
@@ -0,0 +1,33 @@
+import Command from '../../Classes/Command';
+import Neptune from '../../Classes/Neptune';
+
+import path from 'path';
+import {
+    Message,
+    MessageEmbed
+} from 'discord.js';
+import Util from '../../Classes/Util';
+
+export default class Cmd extends Command {
+    constructor(nep: Neptune) {
+        let cmd = `${nep.prefix}${path.basename(__filename, '.ts')}`;
+
+        super(nep, {
+            name: path.basename(__filename, '.ts'),
+            help: `Play the queue or song.`,
+            long_help: `Plays the current queue or plays a link or search term.`,
+            usage: [`- ${cmd} [Search/Link]`],
+            examples: [`- ${cmd}`, `- ${cmd} fallen kingdom`, `-${cmd} https://www.youtube.com/watch?v=dQw4w9WgXcQ`],
+            category: path.dirname(__filename).split(path.sep).pop(),
+            cooldown: 1e3,
+            aliases: ['p'],
+            locked: false,
+            allow_dm: false
+        });
+    }
+
+    public async run(msg: Message, args: any[], util: Util, nep: Neptune) {
+        msg.channel.send('Ok');
+    }
+
+}
